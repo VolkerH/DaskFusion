e1257c424b9e64632a21e7dc4cd574cec39b4bdc
Volker Hilsenstein <volker.hilsenstein@embl.de>
2021-06-25 14:30:50 +0200

    black formatting and removing unused imports

--- utils/chunks.py
+++ utils/chunks.py
@@ -2,46 +2,51 @@ from typing import Tuple, List
 from dask.array.core import normalize_chunks
 import numpy as np
 
-def get_chunk_coordinates(shape: Tuple[int, int], chunk_size: Tuple[int,int], return_np_slice: bool=False):
-    """  Iterator that returns the bounding coordinates
+
+def get_chunk_coordinates(
+    shape: Tuple[int, int], chunk_size: Tuple[int, int], return_np_slice: bool = False
+):
+    """Iterator that returns the bounding coordinates
     for the individual chunks of a dask array of size
     shape with chunk size chunk_size.
-    
-    
+
+
     return_np_slice determines the output format. If True,
-    a numpy slice object is returned for each chunk, that can be used 
+    a numpy slice object is returned for each chunk, that can be used
     directly to slice a dask array to return the desired chunk region.
     If False, a Tuple of Tuples ((row_min, row_max+1),(col_min, col_max+1))
     is returned.
     """
     chunksy, chunksx = normalize_chunks(chunk_size, shape=shape)
-    y=0
+    y = 0
     for cy in chunksy:
-        x =0 
+        x = 0
         for cx in chunksx:
             if return_np_slice:
-                yield np.s_[y:y+cy, x:x+cx]  
+                yield np.s_[y : y + cy, x : x + cx]
             else:
-                yield((y,y+cy), (x,x+cx))
+                yield ((y, y + cy), (x, x + cx))
             x = x + cx
-        y=y + cy
+        y = y + cy
 
 
 def get_rect_from_chunk_slice(chunk_slice):
-    """ given a chunk slice tuple, return a numpy
+    """given a chunk slice tuple, return a numpy
     array that can be added as a shape to napari"
     """
     ylim, xlim = chunk_slice
-    miny, maxy = ylim[0], ylim[1]-1
-    minx, maxx = xlim[0], xlim[1]-1
-    return np.array([[miny, minx],[maxy, minx], [maxy, maxx], [miny,maxx]])
+    miny, maxy = ylim[0], ylim[1] - 1
+    minx, maxx = xlim[0], xlim[1] - 1
+    return np.array([[miny, minx], [maxy, minx], [maxy, maxx], [miny, maxx]])
 
 
-def tile_chunk_intersections(mosaic_shifted: 'shapely.geometry.GeometryCollection',
-                            files: List[str], 
-                            transforms: List[np.ndarray], 
-                            chunks_shapely, 
-                            chunk_slices):
+def tile_chunk_intersections(
+    mosaic_shifted: "shapely.geometry.GeometryCollection",
+    files: List[str],
+    transforms: List[np.ndarray],
+    chunks_shapely,
+    chunk_slices,
+):
     """
     Finds intersections between image tiles and chunks
 
@@ -56,11 +61,9 @@ def tile_chunk_intersections(mosaic_shifted: 'shapely.geometry.GeometryCollectio
     chunk_tiles = {}
     for i, tile in enumerate(mosaic_shifted):
         for j, (chunk, chunk_slice) in enumerate(zip(chunks_shapely, chunk_slices)):
-            anchor_point = (chunk_slice[0][0], chunk_slice[1][0])            
+            anchor_point = (chunk_slice[0][0], chunk_slice[1][0])
             if anchor_point not in chunk_tiles.keys():
                 chunk_tiles[anchor_point] = []
             if tile.intersects(chunk):
                 chunk_tiles[anchor_point].append((files[i], transforms[i]))
     return chunk_tiles
-
- 
--- utils/shapely_and_napari_utils.py
+++ utils/shapely_and_napari_utils.py
@@ -2,14 +2,18 @@ import numpy as np
 from typing import Union, Tuple, Sequence, List
 import copy
 from itertools import product
-from shapely.affinity import affine_transform, translate, scale
+from shapely.affinity import translate
 from shapely.geometry.polygon import Polygon
 from shapely.geometry.base import BaseGeometry
-from shapely.geometry import  GeometryCollection, LineString
+from shapely.geometry import GeometryCollection, LineString
 from skimage.transform import AffineTransform
 
 
-def create_grid_of_shapes(grid_shape: Tuple[int, int], grid_spacing: Tuple[float, float], elements: Union[BaseGeometry, Sequence[BaseGeometry]]) -> List[BaseGeometry]:
+def create_grid_of_shapes(
+    grid_shape: Tuple[int, int],
+    grid_spacing: Tuple[float, float],
+    elements: Union[BaseGeometry, Sequence[BaseGeometry]],
+) -> List[BaseGeometry]:
     """Create a grid of elements
 
     Args:
@@ -23,10 +27,14 @@ def create_grid_of_shapes(grid_shape: Tuple[int, int], grid_spacing: Tuple[float
 
     grid = []
     spacing = np.array(grid_spacing)
-    if not hasattr(type(elements), '__iter__'):
-        elements = [copy.deepcopy(elements) for i in range(grid_shape[0]*grid_shape[1])]
-    for ((y, x), e) in zip(product(range(grid_shape[1]), range(grid_shape[0])), elements):
-        displacement = np.array((x,y)) * spacing
+    if not hasattr(type(elements), "__iter__"):
+        elements = [
+            copy.deepcopy(elements) for i in range(grid_shape[0] * grid_shape[1])
+        ]
+    for ((y, x), e) in zip(
+        product(range(grid_shape[1]), range(grid_shape[0])), elements
+    ):
+        displacement = np.array((x, y)) * spacing
         grid.append(translate(e, displacement[0], displacement[1]))
     return grid
 
@@ -52,26 +60,27 @@ def calculate_transform(src, dst, model_class=AffineTransform):
     return model
 
 
-def napari_shape_to_shapely(coords: np.ndarray, shape_type: str="polygon"):
-    """ 
+def napari_shape_to_shapely(coords: np.ndarray, shape_type: str = "polygon"):
+    """
     Convert an individual napari shape from a shapes layer to a shapely object
-    
-    
-    There is no direct correspondence between a 'rectangle' in napari and a 'box' in 
+
+
+    There is no direct correspondence between a 'rectangle' in napari and a 'box' in
     shapely. The command for creating a box only requires minimum and maximum x and y
     coordinates, i.e. this is for axis aligned rectangles. In contrast, a napari rectangle
-    can be arbitrarily rotated and still be a rectangle. 
+    can be arbitrarily rotated and still be a rectangle.
     So for converting a rectangle from napari to shapely we have to go to a polygon
     """
-    _coords  = coords[:,::-1].copy() # shapely has col,row order, numpy row,col
-    _coords[:,1] *= -1 # axis direction flipped between shapely and napari
+    _coords = coords[:, ::-1].copy()  # shapely has col,row order, numpy row,col
+    _coords[:, 1] *= -1  # axis direction flipped between shapely and napari
     if shape_type in ("rectangle", "polygon", "ellipse"):
         return Polygon(_coords)
     elif shape_type in ("line", "path"):
         return LineString(_coords)
     else:
         raise ValueError
-        
+
+
 def napari_shape_layer_to_shapely(s):
     """
     Convert all shapes in a napari shape layer to shapely objects
@@ -82,29 +91,32 @@ def napari_shape_layer_to_shapely(s):
         shapes.append(napari_shape_to_shapely(_coord, _st))
     return GeometryCollection(shapes)
 
+
 def get_transformed_bbox(shape, affine_matrix: np.ndarray) -> np.ndarray:
     """
     returns the corner coordinates  of a 2D array with shape shape
     after applying the affine transform transform.
     This corresponds to the oriented bounding box
     """
-    h,w = shape
+    h, w = shape
     # create homogeneous coordinates for corner points
-    baserect = np.array([[0,0],[h,0],[h,w],[0,w]])
-    augmented_baserect = np.concatenate((baserect,np.ones((baserect.shape[0],1))), axis=1)
-    # see where the corner points map to 
-    transformed_rect = (affine_matrix @ augmented_baserect.T).T[:,:-1]
+    baserect = np.array([[0, 0], [h, 0], [h, w], [0, w]])
+    augmented_baserect = np.concatenate(
+        (baserect, np.ones((baserect.shape[0], 1))), axis=1
+    )
+    # see where the corner points map to
+    transformed_rect = (affine_matrix @ augmented_baserect.T).T[:, :-1]
     return transformed_rect
+
 
 def get_image_layer_rect(layer):
     """given a napari image or labels layer return the oriented bounding box
     coordinates that can be added to a napari shape layer"""
     im = layer.data
-    if layer.data.ndim>2:
-        im=np.squeeze(im)
-    
+    if layer.data.ndim > 2:
+        im = np.squeeze(im)
+
     if im.ndim != 2:
         raise ValueError("Layer is not a 2D single channel layer")
-    
-    return get_transformed_bbox(im.shape, layer.affine.affine_matrix)
 
+    return get_transformed_bbox(im.shape, layer.affine.affine_matrix)
