c488e7c901446349f6616f1d53cfd4738abd8784
Volker Hilsenstein <volker.hilsenstein@embl.de>
2021-07-02 13:57:17 +0200

    allow fuse functions to take in images rather than paths to images

--- fuse/fuse.py
+++ fuse/fuse.py
@@ -2,13 +2,13 @@ from typing import Dict
 import numpy as np
 from skimage.transform import AffineTransform
 from scipy.ndimage import affine_transform
-from typing import Dict, Tuple, List, Callable
+from typing import Dict, Tuple, List, Callable, Union, Optional
 from pathlib import Path
 from utils.imutils import load_image, transpose, select_channel, crop_black_border
 from skimage.io import imread
 
-def fuse_func(input_tile_info: Dict[Tuple[int, int], List[Tuple[str, np.ndarray]]],
-         imload_func: Callable = imread,
+def fuse_func(input_tile_info: Dict[Tuple[int, int], List[Tuple[Union[str, np.ndarray], np.ndarray]]],
+         imload_func: Optional[Callable] = imread,
          block_info = None, 
          dtype = np.uint16,
          ) -> np.ndarray:
@@ -32,8 +32,15 @@ def fuse_func(input_tile_info: Dict[Tuple[int, int], List[Tuple[str, np.ndarray]
     tiles_info = input_tile_info[anchor_point]
     print(f"Processing chunk at {anchor_point}")
     fused = np.zeros(chunk_shape, dtype=dtype)
-    for tile_path, tile_affine in tiles_info:
-        im = imload_func(tile_path)
+    for image_representation, tile_affine in tiles_info:
+        if imload_func is not None: 
+            # when imload_func is provided we assume we
+            # have been given strings representing files
+            tile_path = image_representation
+            im = imload_func(tile_path)
+        else:
+            # without imload function we assume images are passed
+            im = image_representation
         shift = AffineTransform(translation=(-anchor_point[0], -anchor_point[1]))
         tile_shifted = affine_transform(im, matrix=np.linalg.inv(tile_affine@shift.params), output_shape=chunk_shape)
         stack = np.stack([fused,tile_shifted])
