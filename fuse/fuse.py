c23829dc2c63ebc2f649b393e4185c78bbd70d7d
Volker Hilsenstein <volker.hilsenstein@embl.de>
2021-10-28 13:06:00 +0200

    Updated type hints, black formatting

--- fuse/fuse.py
+++ fuse/fuse.py
@@ -1,41 +1,54 @@
-from typing import Dict
-import numpy as np
-from skimage.transform import AffineTransform
-from scipy.ndimage import affine_transform
-from typing import Dict, Tuple, List, Callable
 from pathlib import Path
-from utils.imutils import load_image, transpose, select_channel, crop_black_border
+from typing import Callable, Dict, List, Optional, Tuple, Union
+
+import numpy as np
+from scipy.ndimage import affine_transform
 from skimage.io import imread
+from skimage.transform import AffineTransform
+from utils.imutils import (crop_black_border, load_image, select_channel,
+                           transpose)
 
-def fuse_func(input_tile_info: Dict[Tuple[int, int], List[Tuple[str, np.ndarray]]],
-         imload_func: Callable = imread,
-         block_info = None, 
-         dtype = np.uint16,
-         ) -> np.ndarray:
-    
-    """Fuses the tiles that intersect the current chunk of a dask array
-    using maximum projection.
-    
-    Pass this function to dask.array.map_blocks,
-    after partial evaluation of the required image_folder
-    and (if needed) optional arguments.
 
-    
-    Returns:
-        np.ndarray: array of chunk-shape containing max projection of 
-                    tiles falling into chunk
+def fuse_func(
+    input_tile_info: Dict[
+        Tuple[int, int], List[Tuple[Union[str, Path, np.ndarray], np.ndarray]]
+    ],
+    imload_fn: Optional[Callable] = imread,
+    block_info=None,
+    dtype=np.uint16,
+) -> np.ndarray:
+
     """
-    array_location = block_info[None]['array-location']
-    # the anchor point is the key to the input_tile_info dictionary
+    Fuses the tiles that intersect the current chunk of a dask array using maximum projection.
+
+    Pass this function to dask.array.map_blocks, after partial evaluation of the required
+    image_folder and (if needed) optional arguments.
+
+    Returns:
+        Array of chunk-shape containing max projection of tiles falling into chunk
+    """
+    array_location = block_info[None]["array-location"]
+    # The anchor point is the key to the input_tile_info dictionary
     anchor_point = (array_location[0][0], array_location[1][0])
-    chunk_shape = block_info[None]['chunk-shape']
+    chunk_shape = block_info[None]["chunk-shape"]
     tiles_info = input_tile_info[anchor_point]
     print(f"Processing chunk at {anchor_point}")
     fused = np.zeros(chunk_shape, dtype=dtype)
-    for tile_path, tile_affine in tiles_info:
-        im = imload_func(tile_path)
+    for image_representation, tile_affine in tiles_info:
+        if imload_fn is not None:
+            # When imload_fn is provided we assume we have been given strings representing files
+            tile_path = image_representation
+            im = imload_fn(tile_path)
+        else:
+            # Without imload function we assume images are passed
+            im = image_representation
         shift = AffineTransform(translation=(-anchor_point[0], -anchor_point[1]))
-        tile_shifted = affine_transform(im, matrix=np.linalg.inv(shift.params@tile_affine), output_shape=chunk_shape)
-        stack = np.stack([fused,tile_shifted])
-        fused = np.max(stack,axis=0)
+        tile_shifted = affine_transform(
+            im,
+            matrix=np.linalg.inv(shift.params @ tile_affine),
+            output_shape=chunk_shape,
+            cval=0,
+        )
+        stack = np.stack([fused, tile_shifted])
+        fused = np.max(stack, axis=0)
     return fused
