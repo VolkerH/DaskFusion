1970cbdbe80e520dfd807f430b3936eba6f2f37e
Volker Hilsenstein <volker.hilsenstein@embl.de>
2021-11-01 18:00:24 +0100

    remove unused/unrelated code

--- utils/imutils.py
+++ utils/imutils.py
@@ -1,15 +1,14 @@
-import json
-import os
-import sys
 from pathlib import Path
-from typing import Any, Callable, Dict, Iterable, List, Optional, Union
+from typing import Callable, List, Union
 
 import numpy as np
 from napari.layers import Image
 from skimage.io import imread
 
 FilePath = Union[Path, str]
-ArrayLike = Union[np.ndarray]  # could add other array types if needed
+ArrayLike = Union[
+    np.ndarray, "dask.array.Array"
+]  # could add other array types if needed
 
 
 def transpose(array: ArrayLike) -> ArrayLike:
@@ -22,7 +21,7 @@ def select_channel(array: ArrayLike, channel=0) -> ArrayLike:
 
 def crop_black_border(array: ArrayLike, border_width: int = 12) -> ArrayLike:
     """
-    Crops away the band of black pixels on our nikon camera.
+    Crops away the band of black pixels on the Nikon camera used in our lab.
     """
     right = -border_width if border_width > 0 else None
     return array[:, :right]
@@ -45,33 +44,9 @@ def load_image(
     file: FilePath, transforms: List[Callable[[ArrayLike], ArrayLike]] = None
 ) -> np.ndarray:
     img = imread(file)
-    #if img.ndim == 2:
+    # if img.ndim == 2:
     #    img = np.expand_dims(img, axis=0)
     if transforms is not None:
         for t in transforms:
             img = t(img)
     return img
-
-
-def create_image_layer_name(path: Path, channel: Optional[int] = None) -> str:
-    if channel is None:
-        return path.stem
-    else:
-        return f"{path.stem}_[{channel}]"
-
-
-def find_layer_by_metadata(
-    layers: Iterable[Image], metadata: Dict[str, Any]
-) -> Optional[Image]:
-    candidates = [
-        l
-        for l in layers
-        if all(
-            k in l.metadata and l.metadata[k] == v for k, v in metadata.items()
-        )
-    ]
-    assert (
-        len(candidates) <= 1
-    ), "Multiple matching layers found. Every Layer should be unique for given metadata."
-    return candidates[0] if candidates else None
-
